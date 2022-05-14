c15907981f7da7bb79337bf1885185909d803bcd
babornat <natya099@gmail.com>
2022-05-14 12:53:11 +0200

    add a delete

--- src/components/SingleProducts.js
+++ src/components/SingleProducts.js
@@ -1,6 +1,50 @@
-const SingleProducts = ({prod}) =>{
+import { Card, Button} from "react-bootstrap";
+import Rating from "./Rating";
+import { CartState } from "../context/Context";
+
+    const SingleProducts = ({ prod }) => {
+        const {
+          state: { cart },
+          dispatch,
+        } = CartState();
+        
     return(
-        <div>{prod.name}</div>
+        <div className="products">
+            <Card>
+ <Card.Img variant="top" alt={prod.name} src={prod.image}></Card.Img>
+ <Card.Body>
+ <Card.Title>{prod.name}</Card.Title>
+ <Card.Subtitle style={{ paddingBottom: 10 }}>
+            <span>CZK {prod.price.split(".")[0]}</span>
+            {prod.fastDelivery ? (
+              <div>Fast Delivery</div>
+            ) : (
+              <div>4 days delivery</div>
+            )}
+            <Rating rating={prod.ratings} />
+          </Card.Subtitle>
+          {cart.some((p) => p.id === prod.id) ? (
+            <Button
+            onClick={() =>
+                dispatch({
+                  type: "REMOVE_FROM_CART",
+                  payload: prod,
+                })
+              }
+            variant="danger">Remove from Cart</Button>
+          ) : (
+            <Button   onClick={() =>
+                dispatch({
+                  type: "ADD_TO_CART",
+                  payload: prod,
+                })
+              } disabled={!prod.inStock}>
+                 {!prod.inStock ? "Out of Stock" : "Add to Cart"}
+            </Button>
+          )}
+ </Card.Body>
+            </Card>
+        </div>
     )
 }
 export default SingleProducts;
