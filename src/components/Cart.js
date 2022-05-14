adc7e69da0892e9b355dc4caf26908bbf2000277
babornat <natya099@gmail.com>
2022-05-14 16:58:33 +0200

    funkcni kvantita

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -56,6 +56,27 @@ const Cart = () => {
                   </Form.Control>
                   
                             </Col>
+
+                            <Col md={2}>
+                  <Form.Control
+                    as="select"
+                    value={prod.qty}
+                    onChange={(e) =>
+                      dispatch({
+                        type: "CHANGE_CART_QTY",
+                        payload: {
+                          id: prod.id,
+                          qty: e.target.value,
+                        },
+                      })
+                    }
+                  >
+                    {[...Array(prod.inStock).keys()].map((x) => (
+                      <option key={x + 1}>{x + 1}</option>
+                    ))}
+                  </Form.Control>
+                </Col>
+
                             <Col md={2}>
                   <Button
                     type="button"
